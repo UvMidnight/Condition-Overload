bf7a606d554cc5c0c583bfc79b795104ea98844c
uvmidnight <23361732+UvMidnight@users.noreply.github.com>
2021-03-13 00:13:37 -0500

    Update

--- src/main/java/uvmidnight/conditionoverload/ConditionOverload.java
+++ src/main/java/uvmidnight/conditionoverload/ConditionOverload.java
@@ -9,6 +9,8 @@ import org.apache.logging.log4j.Logger;
 import slimeknights.tconstruct.library.modifiers.Modifier;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 @Mod(modid = ConditionOverload.MODID, version = ConditionOverload.VERSION, name = ConditionOverload.NAME, dependencies = "after:tconstruct", acceptedMinecraftVersions = "[1.12, 1.13)")
 public class ConditionOverload {
@@ -26,9 +28,13 @@ public class ConditionOverload {
     public static int multiCap = -1;
     public static boolean isFireIncluded;
 
-    public static boolean addativeEnabled;
-    public static float addativeNumber;
+    public static boolean additiveEnabled;
+    public static float additiveNumber;
+    public static float maxAdditive;
 
+    public static boolean isDebugEnabled;
+    public static ArrayList<String> anyList = new ArrayList<>();
+    public static boolean isWhiteOrBlack;
 
     public static Modifier modConditionOverload = new ModConditionOverload();
     public static Item coItem;
@@ -56,13 +62,19 @@ public class ConditionOverload {
 
     private static void initGeneralConfig(Configuration cfg) {
         cfg.addCustomCategoryComment("General Config", "Config for Condition: Overload");
-        rate = cfg.getFloat("percent_multi", "General Config", 1.3F, 1, 100, "Percent multiplication per negative potion effect. Note that superheat, which gives a bonus for targets set on fire, is a value of 1.35.");
-        potCap = cfg.getInt("potion_cap", "General Config", -1, -1, Integer.MAX_VALUE, "Number of potion effects that will be calculated at max. Set to -1 to disable the cap.");
-        isExpo = cfg.getBoolean("is_exponential", "General Config", true, "If Condition Overload will exponentially rise in power as more potion effects are applied. Setting this to false will cause condition overload to be linear in its scaling. NOTE THAT SETTING ADDITIVE MODE ON WILL OVERRIDE THIS");
-        multiCap = cfg.getInt("multiplier_cap", "General Config", -1, -1, Integer.MAX_VALUE, "The cap on the percent that Condition: Overload will add. For example, a value of 1 would be 1%. Set to -1 to disable the cap.");
+        rate = cfg.getFloat("percent_multi", "General Config", 1.3F, 1, 100, "Direct multiplication per negative potion effect. For comparisons sake, superheat, which gives a bonus for targets set on fire, is a value of 1.35.");
+        potCap = cfg.getInt("potion_cap", "General Config", -1, -1, Integer.MAX_VALUE, "Number of potion effects that will be calculated at max. Set to -1 to disable the cap. Still affects additive mode.");
+        isExpo = cfg.getBoolean("is_exponential", "General Config", true, "If Condition Overload will exponentially rise in power as more potion effects are applied. Setting this to false will cause condition overload to be multiplicative in its scaling. NOTE THAT SETTING ADDITIVE MODE ON WILL OVERRIDE THIS. Formula for damage in exponential mode is [weaponBaseDamage * (flat_bonus_per_potion_effect ^ numNegPotions)]. Formula for damage in exponential mode is [weaponBaseDamage * (flat_bonus_per_potion_effect * numNegPotions)].");
+        multiCap = cfg.getInt("multiplier_cap", "General Config", -1, -1, Integer.MAX_VALUE, "The cap on the percent that Condition: Overload will add. For example, a value of 2 would be 200% of the original damage. Set to -1 to disable the cap. Does not affect additive mode.");
         isFireIncluded = cfg.getBoolean("is_fire_included", "General Config", true, "If this is set to true, fire will count as an effect for Condition Overload.");
+        isDebugEnabled = cfg.getBoolean("debug_mode", "General Config", false, "If debug mode is enabled. Prints out information about potions on the target.");
 
-        addativeEnabled = cfg.getBoolean("is_addative" ,"Additive Mode", false, "If Condition Overload adds on flat damage per potion effect on the enemy, rather then multiplying the damage of the weapon.");
-        addativeNumber = cfg.getFloat("flat_bonus_per_potion_effect", "Additive Mode", 1.5f, 0, 10000, "How much damage Condition Overload adds in additive mode per potion effect. Note that the cap of potions still applies.");
+        isWhiteOrBlack = cfg.getBoolean("whitelist_or_blacklist", "General Config", true, "true = blacklist, false = whitelist");
+        anyList.addAll(Arrays.asList(cfg.getStringList("blacklist", "General Config", new String[] {""}, "negative potion names that will be ignored by the modifier. names can be determined with various mods or debug mode.")));
+
+        cfg.addCustomCategoryComment("Additive Mode", "Additive mode adds a flat amount of damage per stack of potion. MUST BE ENABLED IN CONFIG. Formula for damage is [weaponBaseDamage + flat_bonus_per_potion_effect * numNegPotions]");
+        additiveEnabled = cfg.getBoolean("is_additive" ,"Additive Mode", false, "If Condition Overload adds on flat damage per potion effect on the enemy, rather then multiplying the damage of the weapon.");
+        additiveNumber = cfg.getFloat("flat_bonus_per_potion_effect", "Additive Mode", 1.5f, 0, 10000, "How much damage Condition Overload adds in additive mode per potion effect. Note that the cap of potions still applies.");
+        maxAdditive = cfg.getFloat("max_damage_added_additive", "Additive Mode", -1, -1, Float.MAX_VALUE, "What the bonus damage that additive mode can do. Set to -1 to disable.");
     }
 }
--- src/main/java/uvmidnight/conditionoverload/ModConditionOverload.java
+++ src/main/java/uvmidnight/conditionoverload/ModConditionOverload.java
@@ -1,10 +1,13 @@
 package uvmidnight.conditionoverload;
 
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.potion.PotionEffect;
+import net.minecraft.util.text.TextComponentString;
 import net.minecraft.util.text.translation.I18n;
 import net.minecraft.world.World;
 import net.minecraftforge.common.MinecraftForge;
@@ -30,26 +33,57 @@ public class ModConditionOverload extends ModifierTrait {
     @Override
     public float damage(ItemStack tool, EntityLivingBase player, EntityLivingBase target, float damage, float newDamage, boolean isCritical) {
         //go through the potion effects on the target, see which ones are negative
-        int cappedPotionEffects = ConditionOverload.potCap == -1 ?
-                target.getActivePotionEffects().stream().filter((PotionEffect potionEffect) -> potionEffect.getPotion().isBadEffect()).collect(Collectors.toList()).size() :
-                Math.max(ConditionOverload.potCap, target.getActivePotionEffects().stream().filter((PotionEffect potionEffect) -> potionEffect.getPotion().isBadEffect()).collect(Collectors.toList()).size());
+        int cappedPotionEffects = target.getActivePotionEffects().stream().filter(
+                (PotionEffect potionEffect) -> (potionEffect.getPotion().isBadEffect() && ConditionOverload.isWhiteOrBlack) != ConditionOverload.anyList.contains(potionEffect.getEffectName())).collect(Collectors.toList()).size();
+
+        //if there is a cap, take it into account.
+        if (ConditionOverload.potCap != -1) {
+            cappedPotionEffects = Math.min(ConditionOverload.potCap, cappedPotionEffects);
+        }
 
         //factor in if the target is burning
         cappedPotionEffects += target.isBurning() && ConditionOverload.isFireIncluded ? 1 : 0;
 
-        if (ConditionOverload.addativeEnabled) {
-            return damage + (ConditionOverload.addativeNumber * cappedPotionEffects);
+        //debug stuff
+        if (ConditionOverload.isDebugEnabled && player instanceof EntityPlayer && !player.world.isRemote) {
+            Minecraft mc = Minecraft.getMinecraft();
+            mc.player.sendMessage(new TextComponentString("Number of potion effects (capped): " + cappedPotionEffects));
+            int uncappedPotionEffects = target.getActivePotionEffects().stream().filter((PotionEffect potionEffect) -> potionEffect.getPotion().isBadEffect()).collect(Collectors.toList()).size();
+            mc.player.sendMessage(new TextComponentString("Number of uncapped potion effects: " + uncappedPotionEffects));
+            mc.player.sendMessage(new TextComponentString("Would be Exponential Damage: " + (float) (damage * Math.min( ConditionOverload.multiCap == -1 ? Integer.MAX_VALUE : ConditionOverload.multiCap,
+                    Math.pow(ConditionOverload.rate,  cappedPotionEffects)))));
+            mc.player.sendMessage(new TextComponentString("Would be Multiplicative Damage: " + (float) (damage * Math.min( ConditionOverload.multiCap == -1 ? Integer.MAX_VALUE : ConditionOverload.multiCap,
+                    (1 + (ConditionOverload.rate- 1)  *  cappedPotionEffects)))));
+            mc.player.sendMessage(new TextComponentString("Would be Additive Damage: " + (damage + ConditionOverload.maxAdditive == -1 ? ConditionOverload.additiveNumber * cappedPotionEffects : Math.min((ConditionOverload.additiveNumber * cappedPotionEffects), ConditionOverload.maxAdditive))));
+            //display out potion effects
+            //I have also forgotten how to use streams. probably for the better.
+            for (PotionEffect effect : target.getActivePotionEffects()) {
+                mc.player.sendMessage(new TextComponentString("Potion effect: " + effect.getEffectName()));
+                mc.player.sendMessage(new TextComponentString("Is Negative: " + effect.getPotion().isBadEffect()));
+                if (ConditionOverload.anyList.contains(effect.getEffectName())) {
+                    if (ConditionOverload.isWhiteOrBlack) {
+                        mc.player.sendMessage(new TextComponentString("Potion is blacklisted and would be ignored."));
+                    } else {
+                        mc.player.sendMessage(new TextComponentString("Potion is whitelisted and would be allowed."));
+                    }
+                } else {
+                    if (!ConditionOverload.isWhiteOrBlack) {
+                        mc.player.sendMessage(new TextComponentString("Potion is not whitelisted and would be ignored."));
+                    }
+                }
+            }
+        }
+
+        if (ConditionOverload.additiveEnabled) {
+            return damage + ConditionOverload.maxAdditive == -1 ? ConditionOverload.additiveNumber * cappedPotionEffects : Math.min((ConditionOverload.additiveNumber * cappedPotionEffects), ConditionOverload.maxAdditive);
         } else {
             if (ConditionOverload.isExpo) {
-//            System.out.println("Number of negative potion effects: " +  target.ge tActivePotionEffects().stream().filter((PotionEffect potionEffect) -> potionEffect.getPotion().isBadEffect()).collect(Collectors.toList()).size());
-//            System.out.println("Damage: " + (float) (damage * Math.min( ConditionOverload.multiCap == -1 ? Integer.MAX_VALUE : ConditionOverload.multiCap,
-//                    Math.pow(ConditionOverload.rate,  cappedPotionEffects))));
-                return (float) (damage * Math.min( ConditionOverload.multiCap == -1 ? Integer.MAX_VALUE : ConditionOverload.multiCap,
-                        Math.pow(ConditionOverload.rate,  cappedPotionEffects)));
-            } else {
-                return (float) (damage * Math.min( ConditionOverload.multiCap == -1 ? Integer.MAX_VALUE : ConditionOverload.multiCap,
-                        (1 + (ConditionOverload.rate- 1)  *  cappedPotionEffects)));
-            }
+                    return (float) (damage * Math.min( ConditionOverload.multiCap == -1 ? Integer.MAX_VALUE : ConditionOverload.multiCap,
+                            Math.pow(ConditionOverload.rate,  cappedPotionEffects)));
+                } else {
+                    return (float) (damage * Math.min( ConditionOverload.multiCap == -1 ? Integer.MAX_VALUE : ConditionOverload.multiCap,
+                            (1 + (ConditionOverload.rate- 1)  *  cappedPotionEffects)));
+                }
         }
     }
 
